a3c50390b929dcf71fb6f2a479be1df3915a6132
otariterterashvili <otar.terterashvili.1@btu.edu.ge>
2021-03-08 21:45:56 +0400

    feat: lecute-12

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log("hello world");
+// conditional operators
+/*
+ &&  'and operator'
+ || 'or operator'
+ == 'variable value equals somthing'
+ === 'variable value equals somthing !compares also variable types'
+ > 'Grater than'
+ < 'less than'
+ <= 'less or equal'
+ >= 'Grather or equal'
+ !== 'not equal'
+*/
+var x = undefined
+var y = 11
+
+// var z = x % 2 === 0 || y % 2 !== 0 // x is even or y is odd
+
+// if, else if, else
+
+/*
+if (x % 2 === 1) {
+  console.log('X is odd')
+} else if (x % 2 === 0) {
+  console.log('X is even')
+} else {
+  console.log('X is unknown type')
+}
+*/
+
+//
+
+// var z = x + y
+
+// change variable value
+
+// var z = y - x
+// x = x + z
+// y = y - z
+
+// var c = x
+// x = y
+// y = c
+
+/*
+x = x + y // x = 13
+y = x - y // y = 5
+x = x - y // x = 8
+*/
